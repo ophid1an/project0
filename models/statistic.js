15160d1d268b57fa791a66e24a955ca5c3240f08
ophid1an <dermentzoglou.ioannis@gmail.com>
2017-05-28 16:05:42 +0300

    Add statistic model

---
+++ models/statistic.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const limits = require('../config').limits;
+
+const statisticSchema = Schema({
+    dateStarted: {
+        type: Date,
+        required: true
+    },
+    diff: {
+        type: String,
+        enum: limits.CW_DIFFICULTIES,
+        required: true,
+    },
+    whitesC: {
+        type: Number,
+        min: 1,
+        required: true
+    },
+    player1: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+    p1Letters: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    player2: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true
+    },
+    p2Letters: {
+        type: Number,
+        min: 0
+    }
+});
+
+module.exports = mongoose.model('Statistic', statisticSchema);
