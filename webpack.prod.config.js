7ef2ced92a3784633fd378e6e1d979cf94f8ab56
ophid1an <dermentzoglou.ioannis@gmail.com>
2017-06-03 01:27:11 +0300

    Rename webpack config file

--- webpack.prod.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-  entry: './src/javascripts/game-session.js',
-  output: {
-    filename: 'game-session.js',
-    path: path.resolve(__dirname, 'dist/javascripts')
-  }
-};
